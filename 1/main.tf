3c4f49d785b595a61f27c8df31b8e6c7cf361d5e
Jiyong <netjy122@naver.com>
2024-09-26 11:20:35 +0900

    add

---
+++ 1/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" { 
+region = "ap-northeast-1"  
+}
+
+
+resource "aws_instance" "simple" {
+ami         = "ami-0162fe8bfebb6ea16"
+instance_type = "t3.micro"
+}
